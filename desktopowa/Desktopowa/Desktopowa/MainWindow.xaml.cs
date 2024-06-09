27e75f17e94839d0a422a4a4a2d88c91a0c77a50
Quotar <dorosz.bartosz.praca@gmail.com>
2024-06-09 17:08:30 +0200

    skonczone

---
+++ desktopowa/Desktopowa/Desktopowa/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Desktopowa
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void NumerTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            string numer = NumerTextBox.Text;
+
+            if (string.IsNullOrWhiteSpace(numer))
+                return;
+
+            string zdjeciePath = $"{numer}-zdjecie.jpg";
+            string odciskPath = $"{numer}-odcisk.jpg";
+
+            if (numer == "000" || numer == "111" || numer == "333")
+            {
+                ZdjecieImage.Source = new BitmapImage(new Uri(zdjeciePath, UriKind.Relative)); 
+                OdciskImage.Source = new BitmapImage(new Uri(odciskPath, UriKind.Relative));
+            }
+            else 
+            {
+                ZdjecieImage.Source = null;
+                OdciskImage.Source = null;
+            }
+        }
+
+            private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string imie = ImieTextBox.Text;
+            string nazwisko = NazwiskoTextBox.Text;
+
+            if(string.IsNullOrWhiteSpace(imie) || string.IsNullOrWhiteSpace(nazwisko)){
+                MessageBox.Show("Wprowadź dane", "Błąd", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            string kolorOczu = "";
+            if (NiebieskieRadioButton.IsChecked == true)
+                kolorOczu = "niebieskie";
+            else if (ZieloneRadioButton.IsChecked == true)
+                kolorOczu = "zielone";
+            else if (PiwneRadioButton.IsChecked == true)
+                kolorOczu = "piwne";
+
+            string message = $"{imie} {nazwisko} kolor oczu {kolorOczu}";
+            MessageBox.Show(message, "Dane paszportowe", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+    }
+}
---
+++ konsola/Konsola/Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj numer Pesel: ");
+        String pesel = scanner.nextLine();
+
+        char plec = sprawdzPlec(pesel);
+        String plecStr = (plec == 'K') ? "Kobieta" : "Mężczyzna";
+        System.out.println("Płeć: " + plecStr);
+
+        boolean poprawny = sprawdzSumeKontrolna(pesel);
+        if(poprawny) {
+            System.out.println("Numer Pesel jest poprawny");
+        }
+        else {
+            System.out.println("Numer Pesel jest niepoprawny");
+        }
+
+    }
+
+    public static char sprawdzPlec(String pesel){
+        int cyfraPlec = Character.getNumericValue(pesel.charAt(9));
+        if (cyfraPlec % 2 == 0){
+            return 'K';
+        }
+        else {
+            return 'M';
+        }
+    }
+
+    public static boolean sprawdzSumeKontrolna(String pesel){
+        int[] wagi = {1,3,7,9,1,3,7,9,1,3};
+        int suma = 0;
+
+        for (int i=0; i < 10; i++){
+            suma += Character.getNumericValue(pesel.charAt(i)) * wagi[i];
+        }
+
+        int cyfraKontrolna = Character.getNumericValue(pesel.charAt(10));
+        int modulo = suma % 10;
+        int sumaKontrolna = (modulo == 0) ? 0 : 10 - modulo;
+
+        return sumaKontrolna == cyfraKontrolna;
+    }
+}
